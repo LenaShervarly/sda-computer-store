2131a0b159c74249d2048e9b775c020bfe7a1325
Lena Shervarly <lena.shervarly@gmail.com>
2017-01-30 23:57:11 +0100

    on the way till 3d edition. Need to be fixed

---
+++ computer-store_2edition/src/com/company/Component.java
@@ -0,0 +1,50 @@
+package com.company;
+
+import java.util.ArrayList;
+
+/**
+ * Created by elena on 30.01.2017.
+ */
+public class Component {
+    private String manufacturer;
+    private double cost;
+
+    /**
+     * @return a total cost in SEK
+     */
+    public double getCost()
+    {
+        return cost;
+    }
+    public String getDescription()
+    {
+        return " ";
+    }
+    private ArrayList<Component> components = new ArrayList<>();
+    private int totalCostOfComponents = 0;
+
+    /**
+     *
+     * @return the total cost of all the components within the collection
+     */
+    public int totalCostOfComponents( )
+    {
+        for(Component component : components)
+        {
+            totalCostOfComponents += component.getCost();
+        }
+        return totalCostOfComponents;
+    }
+    public int totalCostOfComponents(Component component_type)
+    {
+        totalCostOfComponents = 0;
+        if(component_type instanceof Processor)
+            totalCostOfComponents += component_type.getCost();
+        if(component_type instanceof Display)
+            totalCostOfComponents += component_type.getCost();
+        if(component_type instanceof HardDisk)
+            totalCostOfComponents += component_type.getCost();
+
+        return totalCostOfComponents;
+    }
+}
